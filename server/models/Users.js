ae5019d709b7292a64ec1340d5d892d2e31a2bbd
Saumyadeep Sarkar <saumyadeeps1@gmail.com>
2019-07-30 21:11:06 +0530

    First Commit

---
+++ server/models/Users.js
@@ -0,0 +1,33 @@
+const Sequalize = require("sequelize")
+const db = require("../db/db")
+
+module.exports = db.sequelize.define(
+    'users',
+    {
+        id: {
+            type: Sequalize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        first_name: {
+            type: Sequalize.STRING
+        },
+        last_name: {
+            type: Sequalize.STRING
+
+        },
+        email: {
+            type: Sequalize.STRING
+        },
+        country: {
+            type: Sequalize.STRING
+        },
+        password: {
+            type: Sequalize.STRING
+        },
+    }
+    // },
+    // {
+    //     timestamp: false
+    // }
+)
